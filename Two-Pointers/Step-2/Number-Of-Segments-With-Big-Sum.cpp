e0017fcc02629bbcfa38a9592a1ae923af5d7745
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2024-10-27 17:53:32 +0530

    Create Number-Of-Segments-With-Big-Sum.cpp

---
+++ Two-Pointers/Step-2/Number-Of-Segments-With-Big-Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve() {
+    ll n, s;
+    cin >> n >> s;
+    vector<ll> a(n);
+
+    for (ll i = 0; i < n; i++) cin >> a[i];
+
+    ll left = 0, sum = 0, count = 0;
+    for (int right = 0; right < n; right++) {
+        sum += a[right];
+        while (sum >= s) {
+            count += (n - right);
+            sum -= a[left];
+            left++;
+        }
+    }
+    cout << count << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
